d41691ebe73915820f802cc2a178e761cd5d66ae
Avinash Tauro <avinash.tauro@arctechinfo.com>
2022-03-07 17:48:32 +0530

    Add project files.

---
+++ Week11Day1Demo/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Week11Day1Demo.Models;
+
+namespace Week11Day1Demo.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
---
+++ Week11Day1Demo/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Week11Day1Demo.Models
+{
+    public class Student
+    {
+        [Key]
+        public int RollNo { get; set; }
+
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+        
+        public double Percentage { get; set; }
+    }
+}
